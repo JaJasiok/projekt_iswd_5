7dd7577de785f3d2942f65d546192ca4525c29ee
Jan Krenz <jasiek.k01@gmail.com>
2024-05-23 14:39:48 +0200

    Models visualization added, three variants chosen

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, f1_score
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
@@ -146,6 +146,19 @@ def AUC(output: torch.FloatTensor, target: torch.LongTensor) -> torch.FloatTenso
     """
     return roc_auc_score(target.detach().numpy(), output.detach().numpy())
 
+def F1(output: torch.FloatTensor, target: torch.LongTensor) -> torch.FloatTensor:
+    """Calculate the F1 score of the model output compared to the target tensor.
+
+    Args:
+        output (torch.FloatTensor): Output tensor from the model.
+        target (torch.LongTensor): Target tensor.
+
+    Returns:
+        float: The F1 score of the model output compared to the target tensor.
+    """
+    output = (output > 0) * 1
+    return f1_score(target.detach().numpy(), output.detach().numpy())
+
 
 class ScoreTracker:
     def __init__(self):
@@ -153,8 +166,9 @@ class ScoreTracker:
         self.losses = []
         self.auc_scores = []
         self.acc_scores = []
+        self.f1_scores = []
 
-    def append(self, loss: float, auc: float, acc: float) -> None:
+    def append(self, loss: float, auc: float, acc: float, f1: float) -> None:
         """
         Append the given loss, auc, and acc scores to the respective lists.
 
@@ -162,13 +176,15 @@ class ScoreTracker:
             loss (float): The loss score to append.
             auc (float): The AUC score to append.
             acc (float): The accuracy score to append.
+            f1 (float): The F1 score to append.
         """
         self.losses.append(loss)
         self.auc_scores.append(auc)
         self.acc_scores.append(acc)
+        self.f1_scores.append(f1)
 
     def add(self, outputs: torch.FloatTensor, labels: torch.LongTensor) -> None:
-        """Calculate and append the loss, auc, and acc scores for the given model outputs
+        """Calculate and append the loss, auc, acc and f1 scores for the given model outputs
             and ground truth labels.
 
         Args:
@@ -178,6 +194,7 @@ class ScoreTracker:
         self.losses.append(Regret(outputs, labels).item())
         self.auc_scores.append(AUC(outputs, labels).item())
         self.acc_scores.append(Accuracy(outputs, labels).item())
+        self.f1_scores.append(F1(outputs, labels).item())
 
 
 def Train(
@@ -230,7 +247,8 @@ def Train(
             scheduler.step()
             acc = Accuracy(outputs, labels)
             auc = AUC(outputs, labels)
-            stats_train.append(loss.item(), auc.item(), acc.item())
+            f1 = F1(outputs, labels)
+            stats_train.append(loss.item(), auc.item(), acc.item(), f1.item())
         with torch.no_grad():
             for _, data in enumerate(test_dataloader, 0):
                 inputs, labels = data
